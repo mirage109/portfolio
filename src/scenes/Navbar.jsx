d2c5a4f03834186efadbd8cff897a76839885b4c
Anastasiia Zibla <zibla.asia@gmail.com>
2024-02-23 18:36:19 +0100

    feat: navbar

---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+import menuIcon from "../assets/menu-icon.svg";
+import closeIcon from "../assets/close-icon.svg";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+  const [isMenuTooggled, setIsMenuTooggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">AZ</h4>
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2 "
+            onClick={() => setIsMenuTooggled(!isMenuTooggled)}
+          >
+            <img alt="menu-icon" src={menuIcon} />
+          </button>
+        )}
+        {!isAboveSmallScreens && isMenuTooggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            <div className="flex justify-end p-12">
+                <button onClick={() => setIsMenuTooggled(!isMenuTooggled)}>
+                <img alt="close-icon" src={closeIcon} />
+                </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;

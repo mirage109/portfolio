4454284d54bdc126b40ba34633ef2c4b81bc7068
Anastasiia Zibla <zibla.asia@gmail.com>
2024-02-27 15:19:59 +0100

    feat: upload

--- src/scenes/Skills.jsx
+++ src/scenes/Skills.jsx
@@ -1,6 +1,5 @@
-import useMediaQuery from "../hooks/useMediaQuery";
+
 const Skills = () => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return <section id="skills" className="pt-10 pb-24"></section>;
 };
